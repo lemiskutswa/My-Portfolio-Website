9981ebdf5ff2b1f96312db6309ef680729824cff
lemiskutswa <kutswalemis16@gmail.com>
2023-03-21 13:25:13 +0300

    transfer script to JS file

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ function closemenu(){
     sidemeu.style.right = "-200px";
 }
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwIY-O2UNbU1zqZQ5kg4AOei4GgAgu2BQPWsroBXx-TT7qnWMgCmai9VbHRfqvPKUOf/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
+
